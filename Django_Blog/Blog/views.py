ff0f34b42f674b9eb5a3702af17bd5469b480532
RiveshManandhar <rivesh.manandhar@avionte.com>
2020-06-15 22:46:37 +0545

    Using templates,passingdata into templates,template inheritance,bootstrap , css with satic and using url_names for links

--- Django_Blog/Blog/views.py
+++ Django_Blog/Blog/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts=[
+    {
+        'aurthor': 'CoreyMS',
+        'Title': 'Blog Post 1',
+        'Content': 'First Post Content',
+        'Date_Posted': 'June 15,2020',
+
+    },
+    {
+        'aurthor': 'Rivesh',
+        'Title': 'Blog Post 2',
+        'Content': 'Second Post Content',
+        'Date_Posted': 'June 01,2020',
+
+    },
+    {
+        'aurthor': 'Sherly',
+        'Title': 'Blog Post 3',
+        'Content': 'Third Post Content',
+        'Date_Posted': 'Jan 15,2020',
+
+    }
+]
 def home(request):
-    return render(request,'Blog/Home.html')
+    context={
+        'post':posts
+    }
+    return render(request,'Blog/Home.html',context)
 def About(request):
-    return render(request,'Blog/About.html')
+    return render(request,'Blog/About.html',{'title':'About'})
