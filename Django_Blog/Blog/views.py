c452413b1c3a589a46644fb1cc94e645ddab483f
RiveshManandhar <rivesh.manandhar@avionte.com>
2020-06-16 23:45:54 +0545

    Creating Database along with tables(models) and sample data and linked the model to the admin page.

--- Django_Blog/Blog/views.py
+++ Django_Blog/Blog/views.py
@@ -1,33 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
-posts=[
-    {
-        'aurthor': 'CoreyMS',
-        'Title': 'Blog Post 1',
-        'Content': 'First Post Content',
-        'Date_Posted': 'June 15,2020',
-
-    },
-    {
-        'aurthor': 'Rivesh',
-        'Title': 'Blog Post 2',
-        'Content': 'Second Post Content',
-        'Date_Posted': 'June 01,2020',
-
-    },
-    {
-        'aurthor': 'Sherly',
-        'Title': 'Blog Post 3',
-        'Content': 'Third Post Content',
-        'Date_Posted': 'Jan 15,2020',
-
-    }
-]
 def home(request):
     context={
-        'post':posts
+        'post':Post.objects.all()
     }
     return render(request,'Blog/Home.html',context)
 def About(request):
