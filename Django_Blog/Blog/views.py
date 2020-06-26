e54435eaf96b71f5cd3f3096a71cf4c9bacf9081
RiveshManandhar <rivesh.manandhar@avionte.com>
2020-06-26 22:30:06 +0545

    Different Kinds of views (list,detail,create,delete,update)

--- Django_Blog/Blog/views.py
+++ Django_Blog/Blog/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
-    context={
-        'post':Post.objects.all()
+    context = {
+        'post': Post.objects.all()
     }
-    return render(request,'Blog/Home.html',context)
+    return render(request, 'Blog/Home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/Home.html'
+    context_object_name = 'post'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+
 def About(request):
-    return render(request,'Blog/About.html',{'title':'About'})
+    return render(request, 'Blog/About.html', {'title': 'About'})
+
